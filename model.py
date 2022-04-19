9e07ea01918f756486d4dcf07d30184bd6f83aa2
Jeeva <jpranesh14@gmail.com>
2022-04-20 01:06:51 +0530

    Initial Commit

---
+++ model.py
@@ -0,0 +1,12 @@
+# from keras.models import load_model
+# import numpy as np
+# import h5py
+
+# model = load_model("new.h5")
+# model._make_predict_function()
+
+# def predict_model(trig):
+#     pred=model.predict(trig)
+#     print(pred)
+
+# predict_model("ADANIPORTS")
